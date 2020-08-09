d962739f88dcd074ccd428a35821f3dff21fa2cf
Muhammad Bhaska <muhammadbhaska0@gmail.com>
2020-08-09 12:12:56 +0700

    Membuat components layout

--- common/components/Layout/index.js
+++ common/components/Layout/index.js
@@ -1,5 +1,27 @@
-import React from 'react';
 import { makeStyles } from '@material-ui/core/styles';
 import PropTypes from 'prop-types';
+import { Children } from 'react';
 
-const Layout = () => {};
+const useStyles = makeStyles({
+  content: {
+    marginTop: 74,
+  },
+});
+
+const Layout = () => {
+  const classes = useStyles();
+  return (
+    <>
+      <div className={classes.content}>{children}</div>
+    </>
+  );
+};
+
+Layout.propTypes = {
+  children: PropTypes.oneOfType([
+    PropTypes.arrayOf(PropTypes.node),
+    PropTypes.node,
+  ]),
+};
+
+export default Layout;
