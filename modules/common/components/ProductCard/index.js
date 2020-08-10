53b6457c26b195407fcd6e64d1f46f0c3ee0cfe0
Muhammad Bhaska <muhammadbhaska0@gmail.com>
2020-08-10 07:56:58 +0700

    Membuat currencyFormatter

--- modules/common/components/ProductCard/index.js
+++ modules/common/components/ProductCard/index.js
@@ -13,6 +13,8 @@ import {
 import PromoLabel from '@common/components/PromoLabel';
 import ProductRating from '@common/components/ProductRating';
 
+import { currencyFormatter } from '@utils/currency';
+
 const useStyles = makeStyles({
   card: {
     maxWidth: 360,
@@ -60,7 +62,7 @@ const ProductCard = ({
                 {title}
               </Typography>
               <Typography variant="overline" className={classes.price}>
-                {price}
+                {currencyFormatter(price)}
               </Typography>
               <ProductRating rating={rating} sold={sold} />
             </Grid>
